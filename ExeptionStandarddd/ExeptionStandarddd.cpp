1c0e1dddbf1271b1e8530e803f4fc5dc86a5486f
harlanfadhilah69 <harlancoegg@gmail.com>
2024-06-04 17:32:25 +0700

    membuka,menunjuk sebuah nama file

--- ExeptionStandarddd/ExeptionStandarddd.cpp
+++ ExeptionStandarddd/ExeptionStandarddd.cpp
@@ -6,5 +6,13 @@ using namespace std;
 int main() {
 	string baris;
 
+	//membuka file dalam mode menulis
+	ofstream outfile;
+	//menunjuk ke sebuah nama file
+	outfile.open("contohfile.txt");
+
+	cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+	//unlimited loop untuk menulis
 	
 }
