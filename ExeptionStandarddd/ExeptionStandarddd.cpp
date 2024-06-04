e688ca82924ed604d8fd7319e5a21565bf5a4327
harlanfadhilah69 <harlancoegg@gmail.com>
2024-06-04 17:32:44 +0700

    while

--- ExeptionStandarddd/ExeptionStandarddd.cpp
+++ ExeptionStandarddd/ExeptionStandarddd.cpp
@@ -14,5 +14,15 @@ int main() {
 	cout << ">= Menulis file, \'q\' untuk keluar" << endl;
 
 	//unlimited loop untuk menulis
+
+	while (true) {
+		cout << "- ";
+		//mendapatkan setiap karakter dalam satu baris
+		getline(cin, baris);
+		//loop akan berhentii jika anda memasukan karakter q
+		if (baris == "q") break;
+		//menulis dan memasukan nilai dari 'baris' ke dalam file
+		outfile << baris << endl;
+	}
 	
 }
